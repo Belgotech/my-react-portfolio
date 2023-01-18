9366e1f8dde55aff1fd4c58343c29da9fb8f9445
Belgotech <Belovedobiora2002@gmail.com>
2023-01-18 05:19:18 -0800

    changes

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Footer.css'
+import { AiFillLinkedin } from 'react-icons/ai';
+import { FaGithubSquare } from 'react-icons/fa';
 import { IoLogoTwitter } from 'react-icons/io';
 import { BsFacebook } from 'react-icons/bs';
 
 const Footer = () => {
     return (
-        <footer id='Footer'className='footer'>
+        <footer id='Footer' className='footer'>
             <h2>BELOVED OBIORA</h2>
             <ul>
                 <li><a href="#Home"> Home</a></li>
@@ -15,8 +17,10 @@ const Footer = () => {
                 <li><a href="#Portfolio"> Portfolio</a></li>
                 <li><a href="#Textimonial"> Testimonials</a></li>
                 <li><a href="#"> Contact</a></li>
-                
+
             </ul>
+            <a href="https://www.linkedin.com/mwlite/in/beloved-obiora-665097236" target="_blank"> <AiFillLinkedin /></a>
+            <a href="https://github.com/Belgotech" target="_blank"><FaGithubSquare /></a>
             <a href="https://twitter.com/Beloved99109954" ><IoLogoTwitter /></a>
             <a href="https://m.me/slendid.king" ><BsFacebook /></a>
             <p>BELOVEDOBIORA.All right reserved</p>
