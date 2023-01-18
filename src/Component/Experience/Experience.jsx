c0113e6dee4cbc45726cebf5752d321734bbbae4
Belgotech <Belovedobiora2002@gmail.com>
2023-01-18 05:22:55 -0800

    changes

--- src/Component/Experience/Experience.jsx
+++ src/Component/Experience/Experience.jsx
@@ -85,7 +85,7 @@ const Experience = () => {
             <article className="experience__details">
             <BsPatchCheckFill className="experience__details-icon"/>
               <div>
-                <h4>REACT</h4>
+                <h4>EXPRESS</h4>
                 <small className="text-light">Intermidate</small>
               </div>
             </article>
