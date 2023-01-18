92d3245dcf5237972d5cf3ae9906b2ad1a755fc8
Belgotech <Belovedobiora2002@gmail.com>
2023-01-18 05:15:26 -0800

    changes

--- src/Component/Header/Headersocials.jsx
+++ src/Component/Header/Headersocials.jsx
@@ -8,7 +8,7 @@ const Headersocials = () => {
     return (
         <div>
             <div className="header__socials">
-                <a href="https//:linkedin.com" target="_blank"> <AiFillLinkedin /></a>
+                <a href="https://www.linkedin.com/mwlite/in/beloved-obiora-665097236" target="_blank"> <AiFillLinkedin /></a>
                 <a href="https://github.com/Belgotech" target="_blank"><FaGithubSquare /></a>
                 <a href="https://twitter.com/Beloved99109954" target="_blank"><IoLogoTwitter /></a>
                 <a href="https://m.me/slendid.king" target="_blank"><BsFacebook /></a>
