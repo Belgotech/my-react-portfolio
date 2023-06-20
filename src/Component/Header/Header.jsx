778de4eb9ccb24ef26dbb7f5d47688457b190d69
belgotech <belovedobiora2002@gmail.com>
2023-06-20 16:30:41 +0100

    App Up

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Headersocials from './Headersocials';
 import "./Header.css";
 import CTA from "./CTA/CTA";
-import me from "../../assets/beloved 3.jpg";
+import me from "../../assets/JUNE (3).png";
 
 const Header = () => {
   return (
