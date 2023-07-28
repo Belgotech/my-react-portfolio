46b0576bc4d994f9a6fcb9e9515843497d61783d
belgotech <belovedobiora2002@gmail.com>
2023-07-28 13:28:16 +0100

    App Update

--- src/Component/About/About.jsx
+++ src/Component/About/About.jsx
@@ -30,7 +30,7 @@ const About = () => {
             <article className="about__card">
               {/* <VscFolderLibrary className="about__icon"/> */}
               <h5>Projects</h5>
-              <small>10+ completed</small>
+              <small>Many completed</small>
             </article>
           </div>
 
